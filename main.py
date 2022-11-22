95f3a707900dd3b797fda64eeac77dedae2f759d
João Pedro <100814579+JoaoPedroRMatias@users.noreply.github.com>
2022-11-22 17:06:10 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import time
 
 time.sleep(1)
 
-with open('txt/ascii.txt', 'r', encoding='utf-8') as f:
-    bottoken = f.read()
+with open('txt/asciiGun.txt', 'r', encoding='utf-8') as f:
+    gun = f.read()
 
-print(bottoken)
+print(gun)
 dec = str(input('Rolar a roleta? (s / n)\n'))
 
 if dec == 'n':
@@ -17,17 +17,47 @@ if dec == 'n':
     time.sleep(3)
 
 if dec == 's':
-    x = randint(1,6)
+    x = randint(0,5)
 
-    if x == 6:
+    if x == 0:
         print('\n---------------------------------------------------------------------')
         print('                            MORTO! Tchau tchau rsrs')
         print('---------------------------------------------------------------------\n')
         time.sleep(3)
-        os.system(f"shutdown /s /t 2")
-    
-    if x != 6:
+        os.system(f"shutdown /s /t 2") #FIM
+        
+    if x != 0:
         print('\n---------------------------------------------------------------------')
         print('                            VIVO!')
         print('---------------------------------------------------------------------\n')
-        time.sleep(3)
+        
+        for i in range(5):
+            nDec = str(input('{} voltas e você vence. Roletar novamente? (s / n)\n'.format(5-i)))
+
+            if nDec == 'n':
+                print('\n---------------------------------------------------------------------')
+                print('                            TCHAU MEDROSO!')
+                print('---------------------------------------------------------------------\n')
+                time.sleep(3)
+                break
+ 
+            if nDec == 's':
+                x = randint(0,5)
+
+                if x == 0:
+                    print('\n---------------------------------------------------------------------')
+                    print('                            MORTO! Tchau tchau rsrs')
+                    print('---------------------------------------------------------------------\n')
+                    time.sleep(3)
+                    os.system(f"shutdown /s /t 2") #FIM
+
+                if x != 0:
+                    print('\n---------------------------------------------------------------------')
+                    print('                            VIVO!')
+                    print('---------------------------------------------------------------------\n')
+
+        print('Parabéns, você saiu vivo!')
+        with open('txt/asciiWin.txt', 'r', encoding='utf-8') as f:
+            win = f.read()
+        print(win)
+        print('\n---------------------------------------------------------------------')
